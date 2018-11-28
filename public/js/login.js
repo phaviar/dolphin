5e7860e28abda9b47e71cbaad2c9b355652b7aaa
HexiaLabs <42822038+HexiaLabs@users.noreply.github.com>
2018-11-27 20:48:08 -0700

    Made textboxes have success

--- public/js/login.js
+++ public/js/login.js
@@ -2,37 +2,42 @@ var usernameRegex = /^[A-Za-z0-9_]{2,32}$/;
 var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,100}$/;
 
 // If they have a token in their local storage then skip login and use that
-window.onload = function() {
+window.onload = function () {
 
 };
 
 function onFormSubmit () {
-    var username = document.getElementById("username_input");
-    var password = document.getElementById("password_input");
+  var username = document.getElementById("username_input");
+  var password = document.getElementById("password_input");
 
-    if (!usernameRegex.test(username.value)) {
-        return username.classList.add("is-danger");
-    }
-    if (!passwordRegex.test(password.value)) {
-        return password.classList.add("is-danger");
-    }
+  if (!usernameRegex.test(username.value)) {
+    return username.classList.add("is-danger");
+  } else {
+    username.classList.add("is-success");
+  }
 
-    var pass = btoa(password.value);
-    sendFormData(username.value, pass);
+  if (!passwordRegex.test(password.value)) {
+    return password.classList.add("is-danger");
+  } else {
+    password.classList.add("is-success");
+  }
+
+  var pass = btoa(password.value);
+  sendFormData(username.value, pass);
 }
 
 function sendFormData (user, pass) {
-    fetch("/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({user: user, pass: pass})
-        }).then(function (response) { return response.json(); })
-        .then(onRequestResponse)
-        .catch(alert);
+  fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ user: user, pass: pass })
+    }).then(function (response) { return response.json(); })
+    .then(onRequestResponse)
+    .catch(alert);
 }
 
 function onRequestResponse (res) {
-    alert(res.ok);
+  alert(res.ok);
 }
